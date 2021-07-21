c81763f6cc261357d0e5449c1afa629c06a09722
Rituraj <84662981+Riturajexe@users.noreply.github.com>
2021-07-21 14:13:10 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  //const HomePage({ Key? key }) : super(key: key);
-
   @override
-
-
-
-
+  
+  //My variables are here
+  
+  //int stands for integer
   int day = 2;
+  //int with a decimal is called double
   double days = 2.5;
-
+  //String is a text
   String name = "Rituraj";
   //num can be used in place of double or int
   num dayss = 3.5;
   //var can be used as a common data type for all type of variables
   var bishu = 'tuesday';
-
-  //const pi = 3.14;
+  //const stand for constant means that once you write const you cant change the value of that variable
+  const pi = 3.14;
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
         appBar: AppBar(title: Text('Scaffold app'),),
  
         body:Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello world! Day_$day by $name"),
+          child: Center(
+            child: Container(
+              child: Text("Hello world! Day_$day by $name"),
           ),
         ),
       ),
-   
-      )
-      );
+    )
+   );
   }
 }
