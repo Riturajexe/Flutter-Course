c41d2cf00d3e80f3d7fefcce9443367bbab820b5
Rituraj Sharma <riturajsharma02007@gmail.com>
2021-08-11 14:32:02 +0530

    Day_12

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_course/models/catalog.dart';
+import 'dart:convert';
 import 'package:flutter_course/widgets/drawer.dart';
 import 'package:flutter_course/widgets/item_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Rituraj";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
@@ -29,7 +50,9 @@ class HomePage extends StatelessWidget {
           child: ListView.builder(
             itemCount: dummyList.length,
             itemBuilder: (context, index) {
-              return itemWidget(item:dummyList[index] ,);
+              return itemWidget(
+                item: dummyList[index],
+              );
             },
           ),
         ));
