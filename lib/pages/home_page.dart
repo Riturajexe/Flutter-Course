37e80fd18825e4068ac253d56d9a93399a551940
FIRST_NAME LAST_NAME <riturajsharma02007@gmail.com>
2021-08-25 19:11:05 +0530

    Day_14

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,6 @@ import 'package:flutter_course/models/catalog.dart';
 import 'package:flutter_course/widgets/drawer.dart';
 import 'package:flutter_course/widgets/item_widget.dart';
 
-
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -44,11 +42,35 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,mainAxisSpacing: 16,crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(12),
+                        child: Text(item.name,style: TextStyle(color: Colors.white),),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+
+                      ),),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        padding: EdgeInsets.all(12),
+                        child: Text(item.price.toString(),style: TextStyle(color: Colors.white),),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+
+                      ),),
+                      ),
+                    );
+                
+                },
                 itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) => itemWidget(
-                  item: CatalogModel.items![index],
-                ),
               )
             : Center(
                 child: CircularProgressIndicator(),
